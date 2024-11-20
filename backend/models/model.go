a8dde49c9ff02960141a61678743801c213da102
om-baji <ombaji124@gmail.com>
2024-11-20 18:24:06 +0530

    backend bootstarping

---
+++ backend/models/model.go
@@ -0,0 +1,64 @@
+package models
+
+import "time"
+
+type User struct {
+	Id       string   `json:"id,omitempty" bson:"_id,omitempty"`
+	Name     string   `json:"name,omitempty" bson:"name,omitempty"`
+	Email    string   `json:"email,omitempty" bson:"email,omitempty"`
+	Password string   `json:"password,omitempty" bson:"password,omitempty"`
+	Account  *Account `json:"account,omitempty" bson:"account,omitempty"`
+	CartId   string   `json:"cartId" bson:"cartId"`
+	IsAdmin  bool     `json:isAdmin" bson:"isAdmin"`
+}
+
+type Account struct {
+	AccountNumber string     `json:"accountNumber" bson:"accountNumber"`
+	Balance       int64      `json:"balance" bson:"balance"`
+	UserId        string     `json:"userId" bson:"userId"`
+	Payments      []*Payment `json:"payments,omitempty" bson:"payments,omitempty"`
+}
+
+type Payment struct {
+	Id     string    `json:"transactionId" bson:"transactionId"`
+	From   string    `json:"from" bson:"from"`
+	To     string    `json:"to" bson:"to"`
+	Amount int64     `json:"amount" bson:"amount"`
+	Time   time.Time `json:"time" bson:"time"`
+	Status string    `json:"status" bson:"status"`
+}
+
+type Cart struct {
+	Id        string     `json:"id" bson:"_id,omitempty"`
+	UserId    string     `json:"userId" bson:"userId"`
+	Products  []CartItem `json:"products" bson:"products"`
+	UpdatedAt time.Time  `json:"updatedAt" bson:"updatedAt"`
+}
+
+type CartItem struct {
+	ProductId string `json:"productId" bson:"productId"`
+	Quantity  int    `json:"quantity" bson:"quantity"`
+}
+
+type Order struct {
+	Id         string      `json:"orderId" bson:"orderId"`
+	UserId     string      `json:"userId" bson:"userId"`
+	Products   []OrderItem `json:"products" bson:"products"`
+	TotalPrice int64       `json:"totalPrice" bson:"totalPrice"`
+	Status     string      `json:"status" bson:"status"`
+	Time       time.Time   `json:"time" bson:"time"`
+}
+
+type OrderItem struct {
+	ProductId string `json:"productId" bson:"productId"`
+	Quantity  int    `json:"quantity" bson:"quantity"`
+	Price     int64  `json:"price" bson:"price"` // Price at the time of purchase
+}
+
+type Product struct {
+	Id          string `json:"productId" bson:"productId"`
+	Title       string `json:"title" bson:"title"`
+	Description string `json:"description" bson:"description"`
+	Price       int64  `json:"price" bson:"price"`
+	Stock       int    `json:"stock" bson:"stock"`
+}
